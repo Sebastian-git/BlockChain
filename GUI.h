cca4f3313d9c43a54fde24d55abb7ac4a1812ae8
Sebastian Cevallos <sebastian01cevallos@gmail.com>
2021-05-19 22:54:40 -0700

    Fixed merge conflicts

--- GUI.h
+++ GUI.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 #include <SFML/Graphics.hpp>
 
@@ -15,21 +14,3 @@ private:
 
 };
 
-=======
-#pragma once
-#include <SFML/Graphics.hpp>
-
-class GUI {
-
-public:
-	GUI();
-
-	void display();
-
-private:
-	sf::Vector2f windowSize;
-	int margin;
-
-};
-
->>>>>>> 5ffb06da6f3de529ca20d021369c8a15fbb79e87
--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include "GUI.h"
 
@@ -9,16 +8,4 @@ int main() {
     gui.display();
 
     return 0;
-=======
-#include <iostream>
-#include "GUI.h"
-
-int main() {
-
-    GUI gui = GUI();
-
-    gui.display();
-
-    return 0;
->>>>>>> 5ffb06da6f3de529ca20d021369c8a15fbb79e87
 }
